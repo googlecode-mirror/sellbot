b126ecdc15852a1a4b0cd870264de860b2fb4824
thetechboy <thetechboy@073b3d9b-bc53-0410-bdab-6d9496dbed8f>
2008-08-05 21:18:30 +0000

    Bot which simply logs in (or fails to).

---
+++ trunk/Configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+	// Dimension: Choose between Atlantean, Rimor, DNW or Test
+	$Dimension = 'Rimor';
+	
+	// The following settings sort of speak for themselves.
+	$AccountName = '';
+	$AccountPassword = '';
+	$ShopCharacter = '';
+	$ShopAdministrator = '';
+
+?>
