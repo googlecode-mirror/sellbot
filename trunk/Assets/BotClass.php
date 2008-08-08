98ab0d9f05727ae1b2b1ab18ec9d42bbf019f43b
thetechboy <thetechboy@073b3d9b-bc53-0410-bdab-6d9496dbed8f>
2008-08-08 07:33:00 +0000

    Some code reshufflings - already refactorng for performance after some tired coding.

--- trunk/Assets/BotClass.php
+++ trunk/Assets/BotClass.php
@@ -5,6 +5,8 @@
 		var $ChatGroups = Array();	
 		var $ChatChannel = NULL;
 		var $Status = Array();
+		var $Users = Array();
+		
 		function Bot( &$AOChat, $ShopCharacter, $ShopAdministrator ) {
 			$this->AOChat = &$AOChat;
 			$this->ShopCharacter = $ShopCharacter;
@@ -12,6 +14,7 @@
 		}
 		
 		function Login( $Dimension, $AccountName, $AccountPassword ) {
+		
 			$Servers = Array (
 				'Atlantean' => Array('chat.d1.funcom.com',7012),
 				'Rimor' => Array('chat.d2.funcom.com', 7012),
@@ -25,45 +28,10 @@
 			$this->AOChat->authenticate( $AccountName, $AccountPassword );
 			$this->AOChat->login( $this->ShopCharacter );
 			
-			$this->SendTell($this->ShopAdministrator, "Your SellBot has logged in successfully. :)");
-			$this->LogonOptions();
-		}
-		
-		function LogonOptions() {
-			$Text = "For options, ";
+			$OptionsBlob = $this->GenerateBlob(BLOB_OPTIONS);			
+			$this->SendTell($this->ShopAdministrator, "Your SellBot has logged in successfully. :: " . $OptionsBlob);
 			
-			$Blob = new ChatBlob("click here.");
-			$Blob->AddText( "To select the channel your shop will be posted on: " );
-			$Blob->AddChatCmd( "/tell " . $this->ShopCharacter . " ChanSelect" );
-			$Blob->Render();
-			
-			$Text .= $Blob->Output;
-			
-			$this->SendTell($this->ShopAdministrator, $Text);
-		}
-		
-		function ChooseChatChannel() {
-			//$this->ChatGroups = $ChatGroups;
-		
-			$Text = "Choose a channel for your shop by clicking "; $Blob = new ChatBlob("here.");
-			$Blob->AddText( "Here is a list of all the channels your SellBot can send messages to. Click the one on which you want your shop posted.<br>" );
-
-			foreach ($this->ChatGroups as $GroupId => $Group){
-				if ($this->ValidateOutputChan($Group)) {
-					$Blob->AddChatCmd( "/tell " . $this->ShopCharacter . " ChanSelect " . $GroupId , $Group[1] );
-					$Blob->AddLineBreak();
-				}
-			}
-			$Blob->Render();
-			$Text .= $Blob->Output;
-			
-			$this->SendTell( $this->ShopAdministrator, $Text );
-		
-		}
-		
-		function ValidateOutputChan($Group) {
-			return (substr(bin2hex($Group[0]), 0, 2) != 86) ? false : true; // first byte is not hex 86 then it is not a shopping channel
-		}
+		}	
 		
 		function ValidateChanMsg() {
 			
@@ -71,56 +39,64 @@
 		
 			/*** Validations *****/
 			if ( !isset( $this->ChatGroups[$i] ) ) {
-				$this->SendTell ( $this->ShopAdministrator, 'Invalid channel ID!' );
+				$this->SendTell( $this->ShopAdministrator, 'Invalid channel ID!' );
 				$fail = true;
-			} elseif ( !$this->ValidateOutputChan( $this->ChatGroups[$i] ) ) {
-				$this->SendTell ( $this->ShopAdministrator, 'This channel is not a shopping channel!' );
+			} elseif ( substr(bin2hex($this->ChatGroups[$i][0]), 0, 2) != 86 ) {
+				$this->SendTell( $this->ShopAdministrator, 'This channel is not a shopping channel!' );
 				$fail = true;
 			}
 			
 			if (isset($fail)) {
 				$this->ChatChannel = NULL;
-				$this->ChooseChatChannel($this->ChatGroups);
+				$this->SendTell( $this->ShopAdministrator, "Choose a channel for your shop by clicking " . $this->GenerateBlob( BLOB_CHOOSECHAN ) );
 			} else {			
-				$this->SendTell ( $this->ShopAdministrator, "Channel " . $this->ChatGroups[$i][1] . ' Selected.' );
+				$this->SendTell( $this->ShopAdministrator, "Channel " . $this->ChatGroups[$i][1] . ' Selected.' );
 			}
+			
 		}
 		
 		function RcvTell( $Args ) {
 			$CharName = $this->AOChat->get_uname( $Args[0] );
-			$Text = $Args[1];
+			$Text = strip_tags($Args[1]);
 			
 			echo "[$CharName]: $Text\n";
 			
+			if ((strtolower(substr($Text, 0, 15)) == "unknown command") ||
+				(strtolower(substr($Text, strlen($CharName)+1, 6)) == "is afk") || 
+				(strtolower(substr($Text, strlen($CharName)+1, 7)) == "is away"))
+				return;
+				
 			$Params = explode(" ", strtolower($Text));
 			
 			switch (strtolower($Params[0])) {
 			
 				case "chanselect":
-					if (!isset($Params[1])) { $this->ChooseChatChannel(); } else {
+					if (!isset($Params[1])) {
+						$this->SendTell( $this->ShopAdministrator, "Choose a channel for your shop by clicking " . $this->GenerateBlob( BLOB_CHOOSECHAN ) );
+					} else {
 						$this->ChatChannel = $Params[1];
 						$this->ValidateChanMsg();
 					}
 					break;
 				
 				case "about":
-					$this->SendTell ( $CharName, "About :: " . $this->GenerateBlob('about') );
+					$this->SendTell( $CharName, "About :: " . $this->GenerateBlob(BLOB_ABOUT) );
 					break;
 				
 				default:
-					$this->SendTell ( $CharName, "Unknown command." );
+					$this->SendTell( $CharName, "Unknown command." );
 					
 			}
 		}
 		
 		function SendTell( $User, $Text ) {
 			$this->AOChat->send_tell( $User, $Text );
-			echo "To [$User]: $Text\n"; 
+			echo "To [$User]: " . strip_tags($Text) . "\n"; 
 		}
 		
-		function GenerateBlob ( $type ) {
-			switch ($type) {
-				case 'about':
+		function GenerateBlob ( $Type ) {
+			switch ($Type) {
+				case BLOB_ABOUT:
 					$Blob = new ChatBlob();
 					$Blob->AddText( "SellBot is a bot developed by Demerzel (RK1: Scottish, RK2: Toxor) ");
 					$Blob->AddText( "designed to enable people to easily create their own personal shop screen. ");
@@ -131,6 +107,31 @@
 					$Blob->AddChatCmd( "/start http://code.google.com/p/sellbot/", "http://code.google.com/p/sellbot/" );
 					$Blob->Render();
 					return $Blob->Output;
+					
+				case BLOB_OPTIONS:
+					$Blob = new ChatBlob("Options");
+					$Blob->AddText( "To select the channel your shop will be posted on: " );
+					$Blob->AddChatCmd( "/tell " . $this->ShopCharacter . " ChanSelect" );
+					$Blob->Render();
+					return $Blob->Output;
+					
+				case BLOB_CHOOSECHAN:
+					$Blob = new ChatBlob("here.");
+					$Blob->AddText( "Here is a list of all the channels your SellBot can send messages to. Click the one on which you want your shop posted.<br>" );
+
+					foreach ($this->ChatGroups as $GroupId => $Group){
+						if ( substr( bin2hex($Group[0]), 0, 2 ) == 86 )  {
+							$Blob->AddChatCmd( "/tell " . $this->ShopCharacter . " ChanSelect " . $GroupId, $Group[1] );
+							$Blob->AddLineBreak();
+						}
+					}
+					$Blob->Render();
+					return $Blob->Output;
+					
+				default:
+					return "(err: unrecognised blob type $Type)";
+					
+					
 
 			}
 		}
